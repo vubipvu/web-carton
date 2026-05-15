a26ae591ecc9c178276b0540d06811e7b1d2562b
vubipvu <vubipthang@gmail.com>
2026-05-15 11:11:10 +0700

    Final: Use personal auth gateway on Vercel

--- api/index.js
+++
@@ -1,5 +0,0 @@
-const { SimpleGatekeeper } = require('simple-gatekeeper');
-module.exports = SimpleGatekeeper({
-  clientId: process.env.OAUTH_CLIENT_ID,
-  clientSecret: process.env.OAUTH_CLIENT_SECRET,
-});
